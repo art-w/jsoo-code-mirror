0fc94befd9ec2e7a6fa061d05ab6167306565e9f
ArthurW <arthur@tarides.com>
2025-05-11 21:27:25 +0200

    Remove markdown and dark theme for lighter bundle

--- includes/includes.js
+++ includes/includes.js
@@ -3,10 +3,8 @@ import { EditorState, Compartment, RangeSet } from "@codemirror/state"
 import { hoverTooltip, lineNumbers, Decoration, WidgetType } from "@codemirror/view"
 import * as lint from "@codemirror/lint"
 import * as autocomplete from "@codemirror/autocomplete"
-import * as dark from "@codemirror/theme-one-dark"
 import * as language from "@codemirror/language"
 import { oCaml } from "@codemirror/legacy-modes/mode/mllike"
-import { markdown } from "@codemirror/lang-markdown"
 
 joo_global_object.__CM__view = EditorView;
 joo_global_object.__CM__state = EditorState;
@@ -19,7 +17,5 @@ joo_global_object.__CM__autocomplete = autocomplete;
 joo_global_object.__CM__hoverTooltip = hoverTooltip;
 joo_global_object.__CM__lineNumbers = lineNumbers;
 joo_global_object.__CM__basic_setup = basicSetup
-joo_global_object.__CM__dark = dark;
 joo_global_object.__CM__stream_parser = language;
 joo_global_object.__CM__mllike = oCaml;
-joo_global_object.__CM__markdown = markdown;
